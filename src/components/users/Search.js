df1d5ac93909dd9ecf2ee79151095ba7f63e4236
Sergio Rosa <scarlosjazz@gmail.com>
2020-05-14 08:17:26 -0400

    chore:  clear users from state

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ export class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onSubmit = e => {
@@ -19,6 +21,7 @@ export class Search extends Component {
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
@@ -35,6 +38,15 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {/* clear button */}
+        {showClear && (
+          <button
+            className='btn btn-light btn-block'
+            onClick={clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
