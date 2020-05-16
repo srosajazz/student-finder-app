fef8aef35b64a0a250c4e67b0f09e49eea57ff30
Sergio Rosa <scarlosjazz@gmail.com>
2020-05-16 16:58:31 -0400

    chore:  moving user state to context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { Fragment, Component } from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-const-assign */
+/* eslint-disable no-undef */
+import React, { Fragment, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -10,18 +13,15 @@ import axios from 'axios';
 import GithubState from './context/github/GithubState';
 import './App.css';
 
-class App extends Component {
-  state = {
-    users: [],
-    user: {},
-    repos: [],
-    loading: false,
-    alert: null,
-  };
+const App = () => {
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState([false]);
+  // eslint-disable-next-line no-unused-vars
+  const [alert, setAlert] = useState(null);
 
   //Get users repos
-  getUserRepos = async username => {
-    this.setState({ loading: true });
+  const getUserRepos = async username => {
+    setLoading(true);
 
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
@@ -30,80 +30,50 @@ class App extends Component {
        {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ repos: res.data, loading: false });
+    setRepos(res.data);
+    setLoading(false);
   };
 
   //Get a sigle student user
-  getUser = async username => {
-    this.setState({ loading: true });
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username} ? client_id=$
-       {process.env.REACT_APP_GITHUB_CLIENT_ID}& client_secret=$
-       {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    this.setState({ user: res.data, loading: false });
-  };
-
-  //Clear users from state
-  clearUsers = () => this.setState({ users: [], loading: false });
 
   //Set alert
-  setAlert = (msg, type) => {
-    this.setState({ alert: { msg, type } });
-    // To remove the alert
-    setTimeout(() => this.setState({ alert: null }), 4000);
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
+    setTimeout(() => setAlert(null), 4000);
   };
 
-  render() {
-    const { users, user, loading, repos } = this.state;
-
-    return (
-      <GithubState>
-        <Router>
-          <div className='App'>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search
-                        clearUsers={this.clearUsers}
-                        showClear={users.length > 0 ? true : false}
-                        setAlert={this.setAlert}
-                      />
-                      <Users loading={loading} users={this.state.users} />
-                    </Fragment>
-                  )}
-                />
-                {/* Route */}
-                <Route exact path='/about' component={About} />
-                <Route
-                  exact
-                  path='/user/:login'
-                  render={props => (
-                    <User
-                      {...props}
-                      getUser={this.getUser}
-                      getUserRepos={this.getUserRepos}
-                      user={user}
-                      repos={repos}
-                      loading={loading}
-                    />
-                  )}
-                />
-              </Switch>
-              <Alert alert={this.state.alert} />
-            </div>
+  return (
+    <GithubState>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search setAlert={showAlert} />
+                    <Users />
+                  </Fragment>
+                )}
+              />
+              {/* Route */}
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={props => (
+                  <User {...props} getUserRepos={getUserRepos} repos={repos} />
+                )}
+              />
+            </Switch>
           </div>
-        </Router>
-      </GithubState>
-    );
-  }
-}
+        </div>
+      </Router>
+    </GithubState>
+  );
+};
 
 export default App;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,14 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useContext } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import propTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+
+  const { loading, users } = githubContext;
+
   if (loading) {
     return <Spinner />;
   } else {
@@ -15,11 +20,6 @@ const Users = ({ users, loading }) => {
       </div>
     );
   }
-};
-
-Users.propTypes = {
-  users: propTypes.array.isRequired,
-  loading: propTypes.bool.isRequired,
 };
 
 const userStyle = {
